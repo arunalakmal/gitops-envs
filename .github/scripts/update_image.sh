ee67b319eac954d70cb32e6adac895559730b538
ArunaLakmal <arunarandunu@gmail.com>
2023-01-14 21:38:33 +0100

    scripted

---
+++ .github/scripts/update_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BOT_NAME=$1
+PATH=$2
+IMG=$3
+EVENT_TYPE=$4
+
+
+git config --global user.name "${BOT_NAME}"
+pushd ${PATH}
+kustomize edit set image ${IMG}:'${EVENT_TYPE}'
+popd
+git add .
+git commit -m "Image updated"
+git push
